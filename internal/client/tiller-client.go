8dc67af6fbad95bdbccca75b691dd7b19939db36
Robert Miethaner <robert.miethaner@est.fujitsu.com>
2017-07-14 17:34:59 +0200

    add update and status functions for releases

--- internal/client/tiller-client.go
+++ internal/client/tiller-client.go
@@ -59,6 +59,17 @@ func (tc *TillerClient) InstallRelease(req *tiller.InstallReleaseRequest) (res *
 	return
 }
 
+// InstallRelease installs a new release
+func (tc *TillerClient) UpdateRelease(req *tiller.UpdateReleaseRequest) (res *tiller.UpdateReleaseResponse, err error) {
+	tc.execute(func(rsc tiller.ReleaseServiceClient) {
+		res, err = rsc.UpdateRelease(tc.context, req)
+		if err != nil {
+			log.Debug("unable to update release")
+		}
+	})
+	return
+}
+
 // UninstallRelease uninstalls a release
 func (tc *TillerClient) UninstallRelease(req *tiller.UninstallReleaseRequest) (res *tiller.UninstallReleaseResponse, err error) {
 	tc.execute(func(rsc tiller.ReleaseServiceClient) {
--- internal/controller/release.go
+++ internal/controller/release.go
@@ -8,6 +8,7 @@ import (
 	tiller "k8s.io/helm/pkg/proto/hapi/services"
 
 	"fmt"
+
 	"github.com/AcalephStorage/rudder/internal/client"
 )
 
@@ -82,6 +83,46 @@ func (rc *ReleaseController) InstallRelease(name, namespace, repo, chart, versio
 	return res, nil
 }
 
+// UpdateRelease updates a release with the provided chart
+func (rc *ReleaseController) UpdateRelease(name, repo, chart, version string, values map[string]interface{}) (*tiller.UpdateReleaseResponse, error) {
+	chartDetails, err := rc.repoController.ChartDetails(repo, chart, version)
+	if err != nil {
+		log.WithError(err).Error("unable to get chart details")
+		return nil, err
+	}
+	tarball := chartDetails.ChartFile
+
+	inChart, err := chartutil.LoadFile(tarball)
+	if err != nil {
+		log.WithError(err).Error("unable to load chart details")
+		return nil, err
+	}
+	raw, _ := yaml.Marshal(values)
+
+	inValues := make(map[string]*hapi_chart.Value)
+	for k, v := range values {
+		inValues[k] = &hapi_chart.Value{Value: fmt.Sprintf("%v", v)}
+	}
+
+	config := &hapi_chart.Config{
+		Raw:    string(raw),
+		Values: inValues,
+	}
+
+	req := &tiller.UpdateReleaseRequest{
+		Name:   name,
+		Chart:  inChart,
+		Values: config,
+	}
+
+	res, err := rc.tillerClient.UpdateRelease(req)
+	if err != nil {
+		log.WithError(err).Error("unable to update release")
+		return nil, err
+	}
+	return res, nil
+}
+
 // UninstallRelease uninstall a release
 func (rc *ReleaseController) UninstallRelease(releaseName string, purge bool) (*tiller.UninstallReleaseResponse, error) {
 	req := &tiller.UninstallReleaseRequest{
@@ -122,4 +163,18 @@ func (rc *ReleaseController) GetRelease(name string, version int32) (*GetRelease
 		Content: content,
 		Status:  status,
 	}, nil
+}
+
+// GetReleaseStatus returns the release status
+func (rc *ReleaseController) GetReleaseStatus(name string, version int32) (*tiller.GetReleaseStatusResponse, error) {
+	req := &tiller.GetReleaseStatusRequest{
+		Name:    name,
+		Version: version,
+	}
+	status, err := rc.tillerClient.GetReleaseStatus(req)
+	if err != nil {
+		log.WithError(err).Error("unable to get release status")
+		return nil, err
+	}
+	return status, nil
 }
